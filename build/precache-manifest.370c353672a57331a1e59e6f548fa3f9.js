afe54f8fe4acb7bcd33a9e306333e66795869037
Daniel Nagai <daniel@shop-office.net>
2020-06-18 21:23:30 +0000

    made changes to the css

---
+++ build/precache-manifest.370c353672a57331a1e59e6f548fa3f9.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbbe58a2db553fbc0bac952f513e9c4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61384adeeedc370f4b19",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "838be901ffe51b63e3fa",
+    "url": "/static/css/main.9348965a.chunk.css"
+  },
+  {
+    "revision": "61384adeeedc370f4b19",
+    "url": "/static/js/2.f7b58ed8.chunk.js"
+  },
+  {
+    "revision": "6d98c309ab0e4d5aa5d30f35f2390953",
+    "url": "/static/js/2.f7b58ed8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "838be901ffe51b63e3fa",
+    "url": "/static/js/main.e03c9b11.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  },
+  {
+    "revision": "b915acd3c78c96039d14759a4929ab8b",
+    "url": "/static/media/bag1.b915acd3.jpg"
+  },
+  {
+    "revision": "12f595034b5cedc282614693b6595075",
+    "url": "/static/media/bag2.12f59503.jpg"
+  },
+  {
+    "revision": "be3960764bd839ba94799df53e404dc5",
+    "url": "/static/media/bag3.be396076.jpg"
+  },
+  {
+    "revision": "ea1a25989e0d6a58461e1025e12e6f56",
+    "url": "/static/media/bag4.ea1a2598.jpg"
+  },
+  {
+    "revision": "04e990d32f76938e78c34e427f13f009",
+    "url": "/static/media/bag5.04e990d3.jpg"
+  },
+  {
+    "revision": "99d176b59080cc70393990801c101549",
+    "url": "/static/media/bag6.99d176b5.jpg"
+  },
+  {
+    "revision": "9777cbae591afa1463ce6b82760ebec7",
+    "url": "/static/media/bag7.9777cbae.webp"
+  },
+  {
+    "revision": "d57665ac9a9a69d57e2e464042274f5a",
+    "url": "/static/media/bag8.d57665ac.jpg"
+  },
+  {
+    "revision": "decb67d93c76e206fa0ddfaa7231bafb",
+    "url": "/static/media/bag9.decb67d9.jpg"
+  },
+  {
+    "revision": "20274a8274b56eb71c5545d7f7038845",
+    "url": "/static/media/nice2.20274a82.jpg"
+  },
+  {
+    "revision": "4eb7e085a9b9aa88a261925f3f8225bd",
+    "url": "/static/media/s1.4eb7e085.jpg"
+  },
+  {
+    "revision": "12091d18892f51377eb6f811ebd13253",
+    "url": "/static/media/s3.12091d18.jpg"
+  }
+]);
